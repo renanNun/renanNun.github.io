a0d5ccc3c6273a3ea1e306d70e70fb18b66055dd
renanNun <renannunes400@gmail.com>
2021-04-08 10:14:54 -0300

    Header implementado

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { UiStyleToggleService } from '../services/ui-style-toggle.service';
+import { TranslocoService } from '@ngneat/transloco';
+declare var $: any;
+
+@Component({
+  selector: 'renan-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit,OnChanges {
+
+  constructor(private uiStyleToggleService: UiStyleToggleService,private transloco: TranslocoService) { }
+
+  private darkMode(){
+
+    var $hamburger = $('.hamburger-inner');
+    var $menu = $('#navbar');
+
+    if(this.uiStyleToggleService.darkTheme()){
+      //Dark Mode no simbolo do hamburguer
+      $hamburger.addClass("dark-mode-hamburguer");
+      //Cores no menu
+      $menu.addClass("dark-mode-menu");
+    }else{
+      $hamburger.removeClass("dark-mode-hamburguer");
+      $menu.removeClass("dark-mode-menu");
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    this.uiStyleToggleService.toggle();
+    this.darkMode();
+  }
+
+  openHamburger(){
+    var $hamburger = $(".hamburger");
+    $hamburger.toggleClass("is-active");
+  }
+  
+  public setActiveLang(lang: string){
+    this.transloco.setActiveLang(lang);
+  }
+
+}
