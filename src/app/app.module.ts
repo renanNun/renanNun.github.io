7f6bf5ef3c3cdc33ff98455e61dc76509cc4bdc1
renanNun <renannunes400@gmail.com>
2021-04-08 08:36:20 -0300

    Criando módulo de errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ErrorsComponent } from './errors/errors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorsComponent
   ],
   imports: [
     BrowserModule,
